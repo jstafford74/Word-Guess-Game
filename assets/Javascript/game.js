876db1c3525e91c67a5d552bacab293fa0688545
Jeremiah Stafford <jstafford@slmdevice.com>
2019-08-24 22:10:35 -0400

    GUI complete

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -4,47 +4,62 @@ window.onload = function () {
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    var level;              // Player chooses level of difficulty 
-    var curLevel;           // Selected level
+    var grade;              // Player chooses level of difficulty 
     var word;               // Selected word
     var guess;              // User's Guess
     var guesses = [];       // Stored guesses
     var counter;            // Count correct guesses
     var space;              // Number of spaces in word '-'
 
-    // Get elements
-
-    var showLevel = document.getElementById("level");
 
     // create alphabet buttons
     var buttons = function () {
         myButtons = document.getElementById('buttons');
         letters = document.createElement('div');
-         letters.setAttribute("id","alphabet");
+        letters.setAttribute("id", "alphabet");
         for (var i = 0; i < alphabet.length; i++) {
             list = document.createElement('button');
-            list.setAttribute("type","button");
-            list.setAttribute("class","col-2 btn btn-dark");
+            list.setAttribute("type", "button");
+            list.setAttribute("class", "col-1 btn btn-dark");
             list.id = 'letter';
             list.innerHTML = alphabet[i].toUpperCase();
-            check();
             myButtons.appendChild(letters);
             letters.appendChild(list);
         }
     };
+    buttons();
+
+    // Select Level
+    grade = [
+        ["hat", "bet", "bear", "blue", "flab", "crib", "stump", "made", "frame", "grape", "track", "stand", "want", "chalk", "track"],
+        ["sharks", "sharp", "arctic", "born", "forward", "form", "forest", "apron", "music", "replied", "began", "anthill", "daylight", "handstand", "sunrise"],
+        ["conflicts", "examples", "helpful", "imagine", "plans", "printed", "problems", "upset", "echoed", "scrambled", "reeds", "valley", "gully", "clutched"]
+    ];
+    //Level Selection
+
+    var basic = document.getElementById("bas");
+    basic.onclick = function () {
+        word = grade[0][Math.floor(Math.random() * 14)];
+        // hide = word.replace(/\s/g, "-");
+        console.log(word);
+    };
+    var inter = document.getElementById("int");
+    inter.onclick = function () {
+        word = grade[1][Math.floor(Math.random() * 14)];
+        // hide = word.replace(/\s/g, "-");
+        console.log(word);
+    };
+    var advan = document.getElementById("adv");
+    advan.onclick = function () {
+        word = grade[2][Math.floor(Math.random() * 13)];
+        // hide = word.replace(/\s/g, "-");
+        console.log(word);
+    };
 
 
-    // Select Catagory
-    var selectLev = function () {
-        if (curLevel === level[0]) {
-            levelName.innerHTML = "Basic";
-        } else if (curLevel === level[1]) {
-            levelName.innerHTML = "Intermediate";
-        } else if (curLevel === level[2]) {
-            levelName.innerHTML = "Advanced";
-        }
-    }
 
+
+    // lvlget();
     // Create guesses ul
     result = function () {
         wordHolder = document.getElementById('hold');
@@ -65,60 +80,44 @@ window.onload = function () {
             wordHolder.appendChild(correct);
             correct.appendChild(guess);
         }
-    }
+    };
+    result();
 
+    // // OnClick Function
+    // check = function () {
+    //     list.onclick = function () {
+    //         var guess = (this.innerHTML);
+    //         this.setAttribute("class", "active");
+    //         this.onclick = null;
+    //         for (var i = 0; i < word.length; i++) {
+    //             if (word[i] === guess) {
+    //                 guesses[i].innerHTML = guess;
+    //                 counter += 1;
+    //             }
+    //         }
 
-    // OnClick Function
-    check = function () {
-        list.onclick = function () {
-            var guess = (this.innerHTML);
-            this.setAttribute("class", "active");
-            this.onclick = null;
-            for (var i = 0; i < word.length; i++) {
-                if (word[i] === guess) {
-                    guesses[i].innerHTML = guess;
-                    counter += 1;
-                }
-            }
+    //     };
 
-        }
+    // };
 
-    }
 
     // Play
-    play = function () {
-        level = [
-            ["hat", "bet", "bear", "blue", "flab", "crib", "stump","made","frame","grape","track","stand","want","chalk","track"],
-            ["sharks", "sharp", "arctic", "born","forward","form","forest","apron","music","replied","began","anthill","daylight","handstand","sunrise"],
-            ["conflicts", "examples", "helpful", "imagine", "plans","printed","problems","upset","echoed","scrambled","reeds","valley","gully","clutched"]
-        ];
 
-        curLevel = level[Math.floor(Math.random() * level.length)];
-        word = curLevel[Math.floor(Math.random() * curLevel.length)];
-        word = word.replace(/\s/g, "-");
-        console.log(word);
-        buttons();
 
-        guesses = [];
-        counter = 0;
-        space = 0;
-        result();
-        comments();
-        selectCat();
-        
-    }
 
-    play();
 
-    
+
+
+
+
 
     // Reset
 
-    document.getElementById('reset').onclick = function () {
-        correct.parentNode.removeChild(correct);
-        letters.parentNode.removeChild(letters);
-        play();
-    };
+    // document.getElementById('reset').onclick = function () {
+    //     correct.parentNode.removeChild(correct);
+    //     letters.parentNode.removeChild(letters);
+    //     play();
+
 };
 
 
