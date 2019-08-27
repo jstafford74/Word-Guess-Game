4dcf8177127ed5256ba354f1dd789aab0edd31c6
Jeremiah Stafford <jstafford@slmdevice.com>
2019-08-27 19:31:02 -0400

    change code

--- assets/Javascript/vowel function program.js
+++ assets/Javascript/vowel function program.js
@@ -6,16 +6,16 @@ var countVowels = function(str){
 		  count++;
     }
 	}
-}
+};
 
 var countVowels = function(str){
 	var count = 0;
 	var input = str.toLowerCase();
-	var vowelArr = [“a”, “e”, “i”, “o”, “u”];
+	var vowelArr = ['a','e','i','o','u'];
 	for (var i = 0; i < input.length; i++){
 	  if(vowelArr.includes(input[i])){
 		  count++;
     }
 	}
 	return count;
-}
+};
